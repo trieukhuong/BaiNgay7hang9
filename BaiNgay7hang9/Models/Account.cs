e14a869f799669d6e26684e038498acf71935df7
trieukhuong <trieukhuong258@gmail.com>
2021-09-14 08:42:03 +0700

    bài ngayf14tang 9

---
+++ BaiNgay7hang9/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BaiNgay7hang9.Models
+{
+    public class Account
+    {
+        public string Usename { get; set }
+        public string Password { get; set }
+
+    }
+}
---
+++ BaiNgay7hang9/Models/LTQLDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BaiNgay7hang9.Models
+{
+    public partial class LTQLDbContext : DbContext
+    {
+        public LTQLDbContext() : Base("name=LTQLDbContext")
+        {
+        }
+        public virtual DbSet<Account> Accounts { get; set; }
+    }
+}
