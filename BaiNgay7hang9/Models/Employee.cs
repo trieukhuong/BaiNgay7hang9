4af2f353c9f18c609c40c00c516088cfcb9b8a89
trieukhuong <trieukhuong258@gmail.com>
2021-09-21 08:44:34 +0700

    anh xa

---
+++ BaiNgay7hang9/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BaiNgay7hang9.Models
+{
+    public class Employee
+    {
+        [key]
+        public string EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public DateTime EmployeeBirth { get; set; }
+    }
+}
